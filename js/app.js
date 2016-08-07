a18f172490e621f2dbfc6f921fc7467adcf3cd90
“lisabattle” <lisabattle@Lisas-MacBook-Pro.local>
2016-08-07 08:53:39 -0500

    debugged text modal, added jquery action to build responsive wireframe

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$( "#headerSubmit" ).click(function() {
+  $( "#wireframe").append( $( "<h3 class='wireframeHeader'>Header</h3>"));
+});
+
+$( "#calendarSubmit" ).click(function() {
+  $( "#wireframe").append( $( "<img src='images/calendar.png' class='wireframeCalendar'>"));
+});
+
+$( "#mediaSubmit" ).click(function() {
+  $( "#wireframe").append( $( "<h3 class='wireframeMedia'>Media Placeholder</h3>"));
+});
+$( "#textSubmit" ).click(function() {
+  $( "#wireframe").append( $( "<p class='wireframeText'>Vestibulum ante ipsizzle check out this izzle faucibizzle yo luctus et gangster posuere owned Curae; Yippiyo dolizzle. Bow wow wow faucibizzle. Brizzle pharetra blandizzle quizzle. Fo shizzle rutrizzle aliquizzle shizzle my nizzle crocodizzle. Phat facilisizzle. Break yo neck, yall sizzle cool, venenatis eu, scelerisque you son of a bizzle, blandizzle ac, magna.</p>"));
+});
+
+$( "#photoSubmit" ).click(function() {
+  $( "#wireframe").append( $( "<img src='images/carousel.png' class='wireframePhoto'>"));
+});
