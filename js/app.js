20c77e5492d974284b25263584630543f81d1034
“lisabattle” <lisabattle@Lisas-MacBook-Pro.local>
2016-08-07 15:41:53 -0500

    taking user input & printing for header, subheader, iframe calendar, iframe youtube

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 $( "#headerSubmit" ).click(function() {
-  $( "#wireframe").append( $( "<h3 class='wireframeHeader'>Header</h3>"));
+	var headerText = $(".headerText").val();
+	var subheaderText = $(".subheaderText").val();
+  $( "#wireframe").append( $( "<h3 class='wireframeHeader'>" + headerText + "</h3>"));
+  $( "#wireframe").append( $( "<h5 class='wireframeHeader'>" + subheaderText + "</h5>"));
 });
 
 $( "#calendarSubmit" ).click(function() {
-  $( "#wireframe").append( $( "<img src='images/calendar.png' class='wireframeCalendar'>"));
+	var iframeCalendar = $(".iframeCalendar").val();
+  $( "#wireframe").append( $( iframeCalendar));
 });
 
 $( "#mediaSubmit" ).click(function() {
-  $( "#wireframe").append( $( "<h3 class='wireframeMedia'>Media Placeholder</h3>"));
+	var youtubeLink = $(".youtubeLink").val();
+  $( "#wireframe").append( $(youtubeLink));
 });
+
 $( "#textSubmit" ).click(function() {
   $( "#wireframe").append( $( "<p class='wireframeText'>Vestibulum ante ipsizzle check out this izzle faucibizzle yo luctus et gangster posuere owned Curae; Yippiyo dolizzle. Bow wow wow faucibizzle. Brizzle pharetra blandizzle quizzle. Fo shizzle rutrizzle aliquizzle shizzle my nizzle crocodizzle. Phat facilisizzle. Break yo neck, yall sizzle cool, venenatis eu, scelerisque you son of a bizzle, blandizzle ac, magna.</p>"));
 });
