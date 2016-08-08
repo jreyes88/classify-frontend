389f132a42a56852ab1678cb13441ac5edb6ea95
“lisabattle” <lisabattle@cpe-172-100-2-227.twcny.res.rr.com>
2016-08-07 19:03:13 -0500

    photo carousel broken, all others working as expected

--- js/app.js
+++ js/app.js
@@ -16,9 +16,17 @@ $( "#mediaSubmit" ).click(function() {
 });
 
 $( "#textSubmit" ).click(function() {
-  $( "#wireframe").append( $( "<p class='wireframeText'>Vestibulum ante ipsizzle check out this izzle faucibizzle yo luctus et gangster posuere owned Curae; Yippiyo dolizzle. Bow wow wow faucibizzle. Brizzle pharetra blandizzle quizzle. Fo shizzle rutrizzle aliquizzle shizzle my nizzle crocodizzle. Phat facilisizzle. Break yo neck, yall sizzle cool, venenatis eu, scelerisque you son of a bizzle, blandizzle ac, magna.</p>"));
+    var wireframeText = ($("textarea#textarea1").val());
+    console.log(wireframeText);
+  $( "#wireframe").append( $('<p>' + wireframeText + '</p>'));
 });
 
 $( "#photoSubmit" ).click(function() {
-  $( "#wireframe").append( $( "<img src='images/carousel.png' class='wireframePhoto'>"));
-});
+  var wireframePhoto1 = $(".image1").val();
+  var wireframePhoto2 = $('.image2').val();
+  var wireframePhoto3 = $('.image3').val();
+  var wireframePhoto4 = $('.image4').val();
+  var wireframePhoto5 = $('.image5').val();
+  // $( "#wireframe").append( $( "<img src='" + wireframePhoto1 + "' class='wireframePhoto'>"));
+  $('#wireframe').append($('<div class="carousel"><a class="carousel-item" href="#one!"><img src="http://lorempixel.com/250/250/nature/1"></a><a class="carousel-item" href="#two!"><img src="http://lorempixel.com/250/250/nature/2"></a><a class="carousel-item" href="#three!"><img src="http://lorempixel.com/250/250/nature/3"></a><a class="carousel-item" href="#four!"><img src="http://lorempixel.com/250/250/nature/4"></a><a class="carousel-item" href="#five!"><img src="http://lorempixel.com/250/250/nature/5"></a></div>'));
+  });
